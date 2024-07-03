45776a71c66abef76eeaf743b5c20279305cf5ce
韩亚琦 <2372684571@qq.com>
2024-07-03 20:46:12 +0800

    Update index.js
    修改×1

---
+++ index.js
@@ -0,0 +1,18 @@
+def square_sum(numbers):
+
+    sum = 0
+
+    for num in numbers:
+
+        sum += num ** 2
+
+    return sum
+
+ 
+
+print(square_sum([1, 2, 3, 4])) # 输出 30
+————————————————
+
+                            版权声明：本文为博主原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接和本声明。
+                        
+原文链接：https://blog.csdn.net/m0_58010546/article/details/135354484
